67bf8244721094238d7c8b511eefd828f9552bed
Rama M <143055322+ramaued@users.noreply.github.com>
2023-08-27 21:02:15 +0700

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import datetime
+import re
+from deta import Deta
+
+DETA_KEY = 'c0ny6eduzgm_88NL7y4Md7hFKqiecmcJ2o4EJGVtE6to'
+
+deta = Deta(DETA_KEY)
+
+db = deta.Base('LoginAuth')
+
+def insert_users(email, username, password):
+    date_joined = str(datetime.datetime.now())
+
+    return db.put({'key': email, 'username': username, 'password': password, 'date_joined': date_joined})
+
+
+def fetch_users():
+    users = db.fetch()
+    return users.items
+
+
+def get_emails():
+    users = db.fetch()
+    emails = []
+    for user in users.items:
+        emails.append(user['key'])
+    return emails
+
+
+def get_usernames():
+    users = db.fetch()
+    usernames = []
+    for user in users.items:
+        usernames.append(user['key'])
+    return usernames
+
+
+def validate_email(email):
+    pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$" #tesQQ12@gmail.com
+
+    if re.match(pattern, email):
+        return True
+    return False
+
+
+def validate_username(username):
+    pattern = "^[a-zA-Z0-9]*$"
+    if re.match(pattern, username):
+        return True
+    return False
+
+
+def sign_up():
+    with st.form(key='signup', clear_on_submit=True):
+        st.subheader('Daftar Akun')
+        email = st.text_input('Email')
+        username = st.text_input('Username')
+        password1 = st.text_input('Password', type='password')
+        password2 = st.text_input('Confirm Password', type='password')
+
+        if email:
+            if validate_email(email):
+                if email not in get_emails():
+                    if validate_username(username):
+                        if username not in get_usernames():
+                            if len(username) >= 2:
+                                if len(password1) >= 6:
+                                    if password1 == password2:
+                                        hashed_password = stauth.Hasher([password2]).generate()
+                                        insert_users(email, username, hashed_password[0])
+                                        st.success('Akun kamu berhasil dibuat!')
+                                    else:
+                                        st.warning('Password tidak cocok!')
+                                else:
+                                    st.warning('Password terlalu pendek!')
+                            else:
+                                st.warning('Username terlalu pendek!')
+                        else:
+                            st.warning('Username sudah dipakai!')
+
+                    else:
+                        st.warning('Username tidak valid!')
+                else:
+                    st.warning('Email sudah dipakai!')
+            else:
+                st.warning('Email tidak valid!')
+
+        st.form_submit_button('Sign Up')
---
+++ streamlit_app.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import pandas as pd
+import streamlit_authenticator as stauth
+from auth import sign_up, fetch_users
+from apriori import runApriori, dataFromFile, to_str_results
+
+st.set_page_config(page_title='Home - MBA Project', layout='wide', initial_sidebar_state='expanded')
+
+try:
+    users = fetch_users()
+    emails = []
+    usernames = []
+    passwords = []
+
+    for user in users:
+        emails.append(user['key'])
+        usernames.append(user['username'])
+        passwords.append(user['password'])
+
+    credentials = {'usernames': {}}
+    for index in range(len(emails)):
+        credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
+        
+    Authenticator = stauth.Authenticate(credentials, "cookie_name", "random_key", cookie_expiry_days=30)
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col2:
+        email, authentication_status, username = Authenticator.login('Masuk Aplikasi', 'main')
+
+    with col3:
+        if not authentication_status:
+            sign_up()
+
+    info1, info2, info3, info4 = st.columns(4)
+
+    if username:
+        if username in usernames:
+            if authentication_status:
+                    with st.sidebar:
+                        st.header("Dashboard")
+                        st.markdown("\n")
+                        #st.subheader(f'Welcome {username}')
+                        default_csv = st.selectbox("Silakan pilih file di bawah", ("mba_dataset.csv", "lainnya"))
+
+                        st.markdown("\n")
+
+                        support_helper = ''' > Support(A) = (Jumlah transaksi mengandung A)/(Transaksi Total) '''
+                        confidence_helper = ''' > Confidence(A->B) = Support(AUB)/Support(A)') '''
+
+                        support = st.slider("Masukkan nilai minimal support", min_value=0.001, max_value=0.9, value=0.03, help=support_helper)
+
+                        st.markdown("\n")
+                            
+                        confidence = st.slider("Masukkan nilai minimal confidence", min_value=0.1, max_value=0.9, value=0.2, help=confidence_helper)
+
+                        st.markdown("---")
+                        Authenticator.logout('Log Out', 'sidebar')
+                        
+                    def main():
+                        # title body
+                        #st.title("Market Basket Analysis dengan Algoritma Apriori")
+                        #st.markdown("\n")
+
+                        df = pd.read_csv(default_csv, header=None, lineterminator="\n")
+                        #st.write(df[0].str.split("\,", expand=True).head())
+
+                        #st.markdown("---")
+
+                        st.subheader("Inputs")
+                        st.markdown('''
+                            **Support** menampilkan transaksi dengan barang yang dibeli bersamaan dalam satu transaksi.\n
+                            **Confidence** menampilkan transaksi di mana barang dibeli satu demi satu.\n
+                            Support dan Confidence untuk Itemsets A and B dapat direpresentasikan berdasarkan rumus.''')
+
+                        st.markdown("---")
+
+                        inFile = dataFromFile(default_csv)
+
+                        items, rules = runApriori(inFile, support, confidence)
+
+                        i, r = to_str_results(items, rules)
+
+                        st.subheader("Results")
+
+                        col1, col2 = st.columns(2)
+
+                        with col1:
+                            st.markdown("**Frequent Itemsets**")
+                            st.write(i)
+
+                        with col2:
+                            st.markdown("**Frequent Rules**")
+                            st.write(r)
+                    main()
+                
+            elif not authentication_status:
+                with info2:
+                    st.error('Password atau Username tidak benar!')
+            else:
+                with info2:
+                    st.error('Silakan masukkan identitas kamu!')
+        else:
+            with info2:
+                st.error('Username tidak ada, silakan Sign Up!')
+
+except:
+    st.warning('Halaman tidak dapat dimuat, coba yang lain!')
