c613db572147ddf33eff5e4f0248249044383be3
Rama M <143055322+ramaued@users.noreply.github.com>
2023-08-29 19:14:36 +0700

    auth.py

--- auth.py
+++ auth.py
@@ -81,10 +81,10 @@ def sign_up():
                             st.warning('Username sudah dipakai!')
 
                     else:
-                        st.warning('Username tidak valid! Gunakan huruf kecil atau angka, dan maskimal 14 karakter.')
+                        st.warning('Username tidak valid! Gunakan huruf kecil atau angka, dan maksimal 14 karakter.')
                 else:
                     st.warning('Email sudah dipakai!')
             else:
-                st.warning('Email tidak valid!')
+                st.warning('Email tidak valid! Masukkan dengan benar.')
 
         st.form_submit_button('Sign Up')
